11483c5b8f30f91c8803311bd62719c258f6ad5d
Lantianyou <dtlantianyou@gmail.com>
2023-08-01 09:59:47 +0800

    feat: remove upload

--- apps/frontend/features/upload.tsx
+++ apps/frontend/features/upload.tsx
@@ -1,10 +1,10 @@
-"use client"
-import { Button } from "@/components/ui/button"
+"use client";
+import { Button } from "@/components/ui/button";
 // import { useEffect, useRef } from 'react'
 // import { FFmpeg, createFFmpeg } from '@ffmpeg/ffmpeg'
 import "@uploadthing/react/styles.css";
 import { UploadButton, UploadDropzone } from "@uploadthing/react";
-import { ArrowUpFromLine } from "lucide-react"
+import { ArrowUpFromLine } from "lucide-react";
 
 // function useFFmpeg() {
 // const ref = useRef<FFmpeg>()
@@ -29,33 +29,10 @@ import { ArrowUpFromLine } from "lucide-react"
 export default function Upload() {
   return (
     <>
-    <Button>
-      <ArrowUpFromLine className="w-4 h-4 mr-4" />
-      Upload
-    </Button>
-      <UploadButton
-        endpoint="imageUploader"
-        onClientUploadComplete={(res) => {
-          // Do something with the response
-          console.log("Files: ", res);
-          alert("Upload Completed");
-        }}
-        onUploadError={(error: Error) => {
-          // Do something with the error.
-          alert(`ERROR! ${error.message}`);
-        }}
-      />
-      <UploadDropzone<any>
-        endpoint="withoutMdwr"
-        onClientUploadComplete={(res) => {
-          // Do something with the response
-          console.log("Files: ", res);
-          alert("Upload Completed");
-        }}
-        onUploadError={(error: Error) => {
-          alert(`ERROR! ${error.message}`);
-        }}
-      />
+      <Button>
+        <ArrowUpFromLine className="w-4 h-4 mr-4" />
+        Upload
+      </Button>
     </>
-  )
+  );
 }
