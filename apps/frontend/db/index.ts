dd52c037d788278a1edadc1f2239a5bb257a1bfc
Lantianyou <dtlantianyou@gmail.com>
2023-08-17 20:36:25 +0800

    feat: add files

--- apps/frontend/db/index.ts
+++ apps/frontend/db/index.ts
@@ -2,6 +2,7 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import { users } from "./schema";
 
+const connectionString = "";
 const client = postgres(connectionString);
 const db = drizzle(client);
 
